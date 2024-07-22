cef78e08027620435586e28527de410627d81668
xna <xna00@qq.com>
2024-07-22 12:08:42 +0800

    Test D1

--- functions/api.ts
+++ functions/api.ts
@@ -1,8 +1,15 @@
 interface Env {
   KV: KVNamespace;
+  DB: D1Database;
 }
 
 export const onRequest: PagesFunction<Env> = async (context) => {
   // const value = await context.env.KV.get("example");
-  return new Response("API");
+  context.env.DB;
+  const { results } = await context.env.DB.prepare(
+    "SELECT * FROM words WHERE word = ?"
+  )
+    .bind("hello")
+    .all();
+  return Response.json(results);
 };
